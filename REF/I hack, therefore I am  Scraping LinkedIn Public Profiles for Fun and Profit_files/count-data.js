90dadad606f2c9b3f09257fae3aaf4259489da13
mliq <liquori@gmail.com>
2014-12-17 16:15:07 -0600

    day 2 commit

---
+++ REF/I hack, therefore I am  Scraping LinkedIn Public Profiles for Fun and Profit_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"http:\/\/blog.ikotler.org\/2012\/12\/scraping-linkedin-public-profiles-for.html","comments":9}]});
+}
